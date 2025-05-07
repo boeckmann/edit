e330effc1973fde05f5baca71091009c04858ac1
Bernd Boeckmann <bernd-git@boeckmann.io>
2025-05-07 21:42:24 +0200

    fix edit window not scrolling up on search

--- source/search.c
+++ source/search.c
@@ -123,7 +123,7 @@ again:
             /* align the window scroll to matching text */
             if (WndCol > ClientWidth(wnd)-1)
                 wnd->wleft = wnd->CurrCol;
-            if (wnd->WndRow > ClientHeight(wnd)-1)    {
+            if (wnd->WndRow != ClientHeight(wnd)-1)    {
                 wnd->wtop = wnd->CurrLine;
                 wnd->WndRow = 0;
             }
